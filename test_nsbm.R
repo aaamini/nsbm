b0533050b05a0765eae03fd4bf81a91b596bef3a
Arash A. Amini <amini.aa@gmail.com>
2021-07-14 11:09:01 -0700

    basic tests

--- test_nsbm.R
+++ test_nsbm.R
@@ -1,7 +1,7 @@
 #set.seed(1234)
 n = 50
 K = 4
-J = 10
+J = 6
 z_tru = sample(1:K, J, replace = T)
 
 L = 5
@@ -12,40 +12,5 @@ for (j in 1:J) {
   A[[j]] = nett::fast_sbm(xi_tru[[j]], eta[[z_tru[j]]])
 }
 
-Rcpp::sourceCpp("hsbm_package/src/utils.cpp", verbose = T)
-
-# `comp_blk_sums` is a function in utils.cpp
-comp_blk_sums(A[[2]], xi_tru[[2]]-1, L) + comp_blk_sums(A[[3]], xi_tru[[3]]-1, L)
-
-comp_m_tensor = function(A, z, xi, L) {
-  K = max(z)
-  lapply(1:K, function(k) 
-    Reduce(`+`, lapply(which(z == k), 
-                       function(j) comp_blk_sums(A[[j]], xi[[j]]-1, L)))
-  )
-}
-
-# Compute the true m tensor
-m_tensor = comp_m_tensor(A, z_tru, xi_tru, L)
-m_tensor
-
-# Effect of changing a single xi
-xi = xi_tru
-s = 5
-j = 8
-xi[[j]][s] = (xi[[j]][s]  + 2) %% L + 1  # change a single label
-
-c(xi[[j]][s], xi_tru[[j]][s] )
-
-# # check that only one element is different
-# lapply(1:J, function(j) sum(xi_tru[[j]] != xi[[j]]))
-
-m_tensor_new = comp_m_tensor(A, z_tru, xi, L)
-lapply(1:K, function(k) m_tensor_new[[k]] - m_tensor[[k]])
-
-# The effect of changing a single z coordinate
-z = z_tru
-z[j] = (z_tru[j] + 1) %% K + 1
-c(z[j], z_tru[j])
-m_tensor_new = comp_m_tensor(A, z, xi_tru, L)
-lapply(1:K, function(k) m_tensor_new[[k]] - m_tensor[[k]])
+source("nathans_nsbm/nSBM_functions.R")
+out = gibbs.nSBM(A, ns = 10, monitor = T)
