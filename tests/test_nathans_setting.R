d41ebd7b96df0156338a497fe3e819e95d121b4f
Arash A. Amini <amini.aa@gmail.com>
2021-07-26 13:30:43 -0700

    splice sampler tests

--- tests/test_nathans_setting.R
+++ tests/test_nathans_setting.R
@@ -10,7 +10,7 @@ set.seed(575)
 niter = 10
 K = L = 10
 n = 40
-m = 6
+m = 60
 
 source("nathans_nsbm/data_gen.R")
 # model = new(NestedSBM, A, K, L)
@@ -25,29 +25,32 @@ source("nathans_nsbm/data_gen.R")
 source("nathans_nsbm/nSBM_functions.R")
 source("nathans_nsbm/spliced_sampler.R")
 
-nreps = 2
-res = do.call(rbind, lapply(1:nreps, function(rep) {
+nreps = 32
+# res = do.call(rbind, lapply(1:nreps, function(rep) {
+res = do.call(rbind, mclapply(1:nreps, function(rep) {
   out = generate_nathans_data(n = n, m = m)
   A = out$A
   z_tru = out$z
   
   res = NULL
-  system.time(samp <- gibbs.nSBM(A, K, L, ns = niter, monitor = TRUE))["elapsed"]
-  # z_init is not relevant to this, but added to make the sequence of the same size.
+  
+  dt = system.time(samp <- gibbs.nSBM(A, K, L, ns = niter, monitor = TRUE))["elapsed"]
   res = rbind(res, data.frame(
     rep = rep,
+    dt = dt,
     iter = 1:(niter + 1), 
     nmi = apply(samp$z, 2, function(z) nett::compute_mutual_info(z, z_tru)),
     method = "NSBM (Nathan's)"))
   
-  system.time(samp <- splice_sampler(A, K, L, ns = niter, monitor = TRUE))["elapsed"]
-  
+  dt = system.time(samp <- splice_sampler(A, K, L, ns = niter, monitor = TRUE))["elapsed"]
   res = rbind(res, data.frame(
     rep = rep,
+    dt = dt,
     iter = 1:(niter + 1), 
     nmi = apply(samp$z, 2, function(z) nett::compute_mutual_info(z, z_tru)),
     method = "Splice"))
-}))
+# }))
+}, mc.cores = 32))
 
 res %>% 
   group_by(iter, method) %>% summarise(nmi = mean(nmi)) %>% 
@@ -63,7 +66,9 @@ res %>%
     # legend.text = ggplot2::element_text(size=18),
   ) + 
   ggplot2::guides(colour = ggplot2::guide_legend(keywidth = 2, keyheight = .75)) +
-  ylab("NMI") + xlab("Iteration") + labs(title = sprintf("m = %d, n = %d", m, n))
+  ylab("NMI") + xlab("Iteration") + labs(title = sprintf("m = %d, n = %d, nreps = %d", m, n, nreps))
 
-# ggsave("test_splice.png", width = 6, height=5)
+ggsave("test_splice2.png", width = 6, height=5)
 
+res %>% 
+  group_by(method) %>% summarise(dt = mean(dt)) 
