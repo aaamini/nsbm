6e7a8551029baf34bcb4a7fbfa1d35ae6e25b000
Arash A. Amini <amini.aa@gmail.com>
2021-07-27 10:08:03 -0700

    tests

--- R/data_gen.R
+++ R/data_gen.R
@@ -1,4 +1,15 @@
-generate_nathans_data = function(n = 40, m = 60) {
+generate_sparse_random_data = function(n = 50, J = 25, lambda = 10, K_tru = 3) {
+  z_tru = sample(1:K_tru, J, replace = T)
+  eta = lapply(1:K_tru, function(i) nett::gen_rand_conn(n, L, lambda = lambda))
+  xi_tru = A = vector("list", J)
+  for (j in 1:J) {
+    xi_tru[[j]] = sample(L, n, replace = T, )
+    A[[j]] = nett::fast_sbm(xi_tru[[j]], eta[[z_tru[j]]])
+  }
+  list(A = A, xi = xi_tru, z = z_tru)
+}
+
+generate_nathans_data = function(n = 40,J = 60) {
   pm1 <- cbind(c(.9, .75, .5)
                , c(.75, .6, .25)
                , c(.5, .25, .1))
@@ -9,19 +20,19 @@ generate_nathans_data = function(n = 40, m = 60) {
                , c(.4, .3, .1)
                , c(.6, .1, .5))
   
-  z_tru <- c(rep(1, m/3), rep(2, m/3), rep(3, m/3))
-  A <- xi_tru <- vector("list", length = m)
-  for (j in seq_len(m)) {
+  z_tru <- c(rep(1,J/3), rep(2,J/3), rep(3,J/3))
+  A <- xi_tru <- vector("list", length =J)
+  for (j in seq_len(J)) {
     
     # vary network order
     # n <- sample(seq(60, 80, 20), 1)
     n <- 40
     
-    if (j <= m/3) {
+    if (j <=J/3) {
       G <- as_adj(sample_sbm(n, pm1, c(.4*n, .35*n, .25*n)))
      xi_tru[[j]] <- c(rep(1, .4*n), rep(2, .35*n), rep(3, .25*n))
       
-    } else if (j <= 2*m/3) {
+    } else if (j <= 2*J/3) {
       G <- as_adj(sample_sbm(n, pm2, c(.7*n, .15*n, .15*n)))
      xi_tru[[j]] <- c(rep(1, .7*n), rep(2, .15*n), rep(3, .15*n))
       
--- tests/test_nathans_setting.R
+++ tests/test_nathans_setting.R
@@ -2,33 +2,35 @@
 library(igraph)
 library(ggplot2)
 library(dplyr)
+library(parallel)
 Rcpp::sourceCpp("src/NestedSBM.cpp", verbose = T)
 setMethod("show", "Rcpp_NestedSBM", function(object) object$print())
+source("R/nSBM_functions.R")
+source("R/splice_sampler.R")
+source("R/data_gen.R")
 
 # simulation ----
 # set.seed(575)
 niter = 10
 K = L = 10
-n = 40
-m = 6
+n_cores = 32
+sparse_data = F
 
-source("R/data_gen.R")
-# model = new(NestedSBM, A, K, L)
-# z_init = model$z + 1
-# # system.time( fitted_model <- model$run_gibbs(niter) )["elapsed"]
-# system.time( fitted_model <- model$run_gibbs_naive(niter) )["elapsed"]
-# 
-# res = data.frame(iter = 1:(niter + 1), 
-#                  nmi = apply(cbind(z_init, fitted_model$z), 2, function(z) nett::compute_mutual_info(z, z_tru)),
-#                  method = "NSBM (C++)")
+if (!sparse_data) {
+  n = 40; J= 60
+} else{
+  n = 50; J = 25; lambda = 10; K_tru = 3
+}
 
-source("R/nSBM_functions.R")
-source("R/splice_sampler.R")
+nreps = 12
+# res = do.call(rbind, lapply(1:nreps, function(rep) {
+res = do.call(rbind, mclapply(1:nreps, function(rep) {
+  if (!sparse_data){
+    out = generate_nathans_data(n = n, J = J) 
+  } else {
+    out = generate_sparse_random_data(n = n, J = J, lambda = lambda, K_tru = K_tru) 
+  }
 
-nreps = 2
-res = do.call(rbind, lapply(1:nreps, function(rep) {
-# res = do.call(rbind, mclapply(1:nreps, function(rep) {
-  out = generate_nathans_data(n = n, m = m)
   A = out$A
   z_tru = out$z
   
@@ -40,7 +42,7 @@ res = do.call(rbind, lapply(1:nreps, function(rep) {
     dt = dt,
     iter = 1:(niter + 1), 
     nmi = apply(samp$z, 2, function(z) nett::compute_mutual_info(z, z_tru)),
-    method = "NSBM (Nathan's)"))
+    method = "Nathan's"))
   
   dt = system.time(samp <- splice_sampler(A, K, L, ns = niter, monitor = TRUE))["elapsed"]
   res = rbind(res, data.frame(
@@ -60,10 +62,12 @@ res = do.call(rbind, lapply(1:nreps, function(rep) {
     iter = 1:(niter + 1), 
     nmi = apply(fitted_model$z, 2, function(z) nett::compute_mutual_info(z, z_tru)),
     method = "Pure C++"))
-}))
-# }, mc.cores = 32))
+# }))
+}, mc.cores = n_cores))
 
-p = res %>% 
+
+state_str =  sprintf("J = %d, n = %d, nreps = %d", J, n, nreps)
+res %>% 
   group_by(iter, method) %>% summarise(nmi = mean(nmi)) %>% 
   ggplot(aes(x = iter, y = nmi, color = method)) + 
   # geom_line(aes(size = method), alpha = 0.5) +
@@ -77,10 +81,13 @@ p = res %>%
     # legend.text = ggplot2::element_text(size=18),
   ) + 
   ggplot2::guides(colour = ggplot2::guide_legend(keywidth = 2, keyheight = .75)) +
-  ylab("NMI") + xlab("Iteration") + labs(title = sprintf("m = %d, n = %d, nreps = %d", m, n, nreps))
+  ylab("NMI") + xlab("Iteration") + labs(title = state_str)
 
-p
-# ggsave("test_splice3.png", width = 6, height=5)
 
-res %>% 
-  group_by(method) %>% summarise(dt = mean(dt)) 
+tag = "sparse"
+ggsave(sprintf("test_splice_new_%s_%s.png", state_str, tag), width = 6, height=5)
+
+knitr::kable( res %>% 
+  filter(iter > niter / 2) %>% 
+  group_by(method) %>% summarise(dt = mean(dt), nmi = mean(nmi)) 
+)
