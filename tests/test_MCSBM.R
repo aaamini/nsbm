7a3a4b7a3a069fa0d6b75f06b0d9214347415bb5
aaamini <amini.aa@gmail.com>
2021-12-10 13:06:47 -0800

    clean up test_MCSBM

--- tests/test_MCSBM.R
+++ tests/test_MCSBM.R
@@ -14,22 +14,6 @@ library(patchwork)
 Rcpp::sourceCpp("src/MCSBM.cpp", verbose = T)
 # Rcpp::sourceCpp("src/SBM.cpp", verbose = T)
 
-
-# # Caompute the optimal matching permutation from zh to z, both label vectors
-# matching_perm = function(zh, z) {
-#   n = length(z)
-#   out = HungarianSolver(1-compute_confusion_matrix(zh, z)/n)
-#   perm = out$pairs[,2]
-#   perm
-# }
-# 
-# comp_all_matching_perms = function(xih, xi_tru) {
-#   lapply(seq_along(xih), function(j) matching_perm(xih[[j]], xi_tru[[j]]))
-# }
-# comp_matching_score = function(xih, xi_tru){
-#   length(unique(comp_all_matching_perms(xih, xi_tru))) # / length(xih)
-# }
-
 # set.seed(1235)
 n = 100
 K = 2
@@ -49,15 +33,6 @@ nathan_data = F
 if (nathan_data) { L = 3 }
 
 methods = list()
-# methods[["SBM"]] = function(A, L, niter=100) {
-#   temp_label_list = lapply(seq_along(A), function(j) {
-#     mod = new(SBM, A[[j]], L, 1, 1)
-#     mod$run_gibbs(niter) 
-#   })
-#   lapply(1:(niter+1), function(iter) 
-#     lapply(seq_along(temp_label_list), function(j) temp_label_list[[j]][,iter])
-#   )
-# }
 
 methods[["MCSBM"]] = function(A, K, L, niter=50) {
 #    out = mix_mcsbm(A, K, L, 1, 1, 0.9, 1, niter, 3)
@@ -188,120 +163,3 @@ p3 = plot_paths_and_avg(res, matching_score) + ylab("Matching score") # + labs(t
 
 print(p1 + p2 + p3)
 ggsave(sprintf("test_mcsbm_%s.png", state_str), width = 10, height=5)
-
-# p1 = res %>% 
-#   group_by(iter, method) %>% summarise(xi_nmi = mean(xi_nmi)) %>% 
-#   ggplot(aes(x = iter, y = xi_nmi, color = method)) + 
-#   # geom_line(aes(size = method), alpha = 0.5) +
-#   geom_line(size = 1.2) + 
-#   theme_minimal() +
-#   # scale_colour_manual(values = c(1,1.5)) +
-#   ggplot2::theme(
-#     legend.background = ggplot2::element_blank(),
-#     legend.title = ggplot2::element_blank(),
-#     legend.position = c(0.8, 0.2),
-#     # legend.text = ggplot2::element_text(size=18),
-#   ) + 
-#   ggplot2::guides(colour = ggplot2::guide_legend(keywidth = 2, keyheight = .75)) +
-#   ylab("xi-NMI") + xlab("Iteration") + labs(title = state_str)
-# 
-# p2 = res %>% 
-#   group_by(iter, method) %>% summarise(z_nmi = mean(z_nmi)) %>% 
-#   ggplot(aes(x = iter, y = z_nmi, color = method)) + 
-#   geom_line(size = 1.2) + 
-#   # geom_violin() +
-#   theme_minimal() +
-#   # scale_colour_manual(values = c(1,1.5)) +
-#   ggplot2::theme(
-#     legend.background = ggplot2::element_blank(),
-#     legend.title = ggplot2::element_blank(),
-#     legend.position = c(0.8, 0.2),
-#     # legend.text = ggplot2::element_text(size=18),
-#   ) + 
-#   ggplot2::guides(colour = ggplot2::guide_legend(keywidth = 2, keyheight = .75)) +
-#   ylab("z-NMI") + xlab("Iteration") + labs(title = state_str)
-# 
-# p3 = res %>% 
-#   group_by(iter, method) %>% summarise(matching_score = mean(matching_score)) %>% 
-#   ggplot(aes(x = iter, y = matching_score, color = method)) + 
-#   # geom_line(aes(size = method), alpha = 0.5) +
-#   geom_line(size = 1.2) + 
-#   theme_minimal() +
-#   # scale_colour_manual(values = c(1,1.5)) +
-#   ggplot2::theme(
-#     legend.background = ggplot2::element_blank(),
-#     legend.title = ggplot2::element_blank(),
-#     legend.position = c(0.8, 0.2),
-#     # legend.text = ggplot2::element_text(size=18),
-#   ) + 
-#   ggplot2::guides(colour = ggplot2::guide_legend(keywidth = 2, keyheight = .75)) +
-#   ylab("Matching score") + xlab("Iteration") + labs(title = state_str)
-# 
-# 
-# p1 + p2 + p3
-# # ggsave(sprintf("test_musbm_%s.png", state_str), width = 10, height=5)
-# 
-# # out = gen_rand_nsbm(n=n, K=K, L=L, J=J,  lambda=lambda, gam = gam, zeta=zeta, sort_z = T)
-# # A = out$A
-# # z_tru = out$z
-# # xi_tru = out$xi
-# # eta = out$eta[[1]]
-# 
-# # # check whether eta is permutation invariant
-# # rand_perm = sample.int(L)
-# # rand_P = as(rand_perm, "pMatrix")
-# # sum((rand_P %*% eta %*% t(rand_P) - eta)^2)
-# 
-# 
-# # # xih_list1 = lapply(seq_along(A), function(j) fit_sbm(A[[j]], L, niter))
-# # xih_list1 = fit_sbm(A, L)
-# # # xih_list1 = lapply(1:(niter+1), function(iter) 
-# # #    lapply(seq_along(xih_list1), function(j) xih_list1[[j]][,iter])
-# # #  )
-# # 
-# # 
-# # xih_list2 = fit_musbm(A, L, niter)
-# # # xih_list2 = lapply(xih_list2, add_one_to_xi)
-# 
-# # compute_nmi_evolution = function(xih_list) {
-# #   sapply(seq_along(xih_list), function(iter) 
-# #     hsbm::get_slice_nmi(xih_list[[iter]], xi_tru))
-# # }
-# # 
-# # iter_seq = 1:(niter+1)
-# # res = data.frame(iter = iter_seq, nmi = compute_nmi_evolution(xih_list1), method = "SBM")
-# # res = rbind(res, 
-# #             data.frame(iter = iter_seq, nmi = compute_nmi_evolution(xih_list2), method = "MuSBM")
-# # )
-# 
-# # res %>% 
-# #   ggplot(aes(x = iter, y = nmi, color = method)) + 
-# #   # geom_line(aes(size = method), alpha = 0.5) +
-# #   geom_line(size = 1.2) + 
-# #   theme_minimal() +
-# #   # scale_colour_manual(values = c(1,1.5)) +
-# #   ggplot2::theme(
-# #     legend.background = ggplot2::element_blank(),
-# #     legend.title = ggplot2::element_blank(),
-# #     legend.position = c(0.8, 0.2),
-# #     # legend.text = ggplot2::element_text(size=18),
-# #   ) + 
-# #   ggplot2::guides(colour = ggplot2::guide_legend(keywidth = 2, keyheight = .75)) +
-# #   ylab("NMI") + xlab("Iteration") 
-# 
-# # hsbm::get_slice_nmi(xih_list2[[niter+1]], xi_tru)
-# # 
-# # xih_final1 = xih_list1[[niter+1]]
-# # xih_final2 = xih_list2[[niter+1]]
-# # 
-# # comp_all_matching_perms = function(xih) {
-# #   lapply(seq_along(xih), function(j) matching_perm(xih[[j]], xi_tru[[j]]))
-# # }
-# # comp_matching_score = function(xih_list){
-# #   length(unique(comp_all_matching_perms(xih_list))) # / length(xih_list)
-# # }
-# # 
-# # length(unique(comp_all_matching_perms(xih_final2)))
-# # length(unique(comp_all_matching_perms(xih_final1)))
-# # 
-# # comp_matching_score(xih_final2)
