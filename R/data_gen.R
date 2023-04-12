4b3372a057d6426d88277b14317f7d1529f49947
Nathaniel Josephs <33576936+njosephs@users.noreply.github.com>
2023-04-12 11:34:04 -0400

    Simplifying SBM simulation

--- R/data_gen.R
+++ R/data_gen.R
@@ -9,17 +9,29 @@ runifmat = function(K, sym = T) {
   if (sym) return(symmetrize(A))
 }
 
-gen_rand_nsbm = function(n = 50, J = 10, 
-                         K = 3, L = 7, 
-                         lambda = 10, 
-                         gam = 0.3, zeta = 0.1, 
-                         pri = rep(1, L) / L) {
+gen_rand_nsbm = function(n, J, K, L
+                         , lambda = 10
+                         , gam = 0.3) {
+  # n : number of nodes per network
+  # J : number of networks per class
+  # K : number of classes
+  # L : number of communities (in classes 1, ..., K)
+  
+  n.L = length(L)
+  
+  if (n.L == 1) {
+    L <- rep(L, K)
+  } else if (n.L != K) {
+    stop("L needs to be a K-length vector")
+  }
   
   # Create connectivity matrices eta[[j]]
+  
   # eta = lapply(1:K, function(j) (1-gam)*rsymperm(L) + gam*runifmat(L))
-  eta = lapply(1:K, function(j) (1-gam)*diag(L) + gam*runifmat(L))
+  eta = lapply(1:K, function(j) (1-gam)*diag(L[j]) + gam*runifmat(L[j]))
+  
   for (k in 1:K) {
-    eta[[k]] = (1-zeta)*eta[[1]] + zeta*eta[[k]] # pull eta[[k]] towards eta[[1]]
+    pri <- rep(1, L[k]) / L[k]
     scale = nett::get_dcsbm_exav_deg(n, pri, eta[[k]], 1)
     eta[[k]] = pmin(eta[[k]] * lambda / scale, 1)
 
@@ -29,9 +41,10 @@ gen_rand_nsbm = function(n = 50, J = 10,
   z = sample(1:K, J, replace = T)
   xi = A = vector("list", J)
   for (j in 1:J) {
-    xi[[j]] = sample(L, n, replace = T, )
+    xi[[j]] = sample(L[z[j]], n, replace = T, )
     A[[j]] = nett::fast_sbm(xi[[j]], eta[[z[j]]])
   }
+  
   list(A = A, eta = eta, xi = xi, z = z)
 }
 
