11b7bbec36c17e3e46c8de4b241527c880df1a00
Nathaniel Josephs <33576936+njosephs@users.noreply.github.com>
2023-06-01 17:58:13 -0400

    Update data_gen.R

--- R/data_gen.R
+++ R/data_gen.R
@@ -11,7 +11,8 @@ runifmat = function(K, sym = T) {
 
 gen_rand_nsbm = function(n, J, K, L
                          , lambda = 10
-                         , gam = 0.3) {
+                         , gam = 0.3
+                         , labeled = TRUE) {
   # n : number of nodes per network
   # J : number of networks per class
   # K : number of classes
@@ -38,65 +39,21 @@ gen_rand_nsbm = function(n, J, K, L
   }
   
   # Sample the labels, z, xi and adjacency matrices A[[j]]
-  z = sample(1:K, J, replace = T)
+  z = rep(1:K, each = ceiling(J/K), length.out = J)
+  # z = sample(1:K, J, replace = T)
   xi = A = vector("list", J)
-  for (j in 1:J) {
-    xi[[j]] = sample(L[z[j]], n, replace = T, )
-    A[[j]] = nett::fast_sbm(xi[[j]], eta[[z[j]]])
+  
+  if (labeled) {
+    for (j in 1:J) {
+      xi[[j]] = rep(1:L[z[j]], each = ceiling(n / L[z[j]]), length.out = n)
+      A[[j]] = nett::fast_sbm(xi[[j]], eta[[z[j]]])
+    }
+  } else {
+    for (j in 1:J) {
+      xi[[j]] = sample(L[z[j]], n, replace = T)
+      A[[j]] = nett::fast_sbm(xi[[j]], eta[[z[j]]])
+    } 
   }
   
   list(A = A, eta = eta, xi = xi, z = z)
-}
-
-
-generate_sparse_random_data = function(n = 50, J = 25, lambda = 10, K_tru = 3) {
-  z_tru = sample(1:K_tru, J, replace = T)
-  eta = lapply(1:K_tru, function(i) nett::gen_rand_conn(n, L, lambda = lambda))
-  xi_tru = A = vector("list", J)
-  for (j in 1:J) {
-    xi_tru[[j]] = sample(L, n, replace = T, )
-    A[[j]] = nett::fast_sbm(xi_tru[[j]], eta[[z_tru[j]]])
-  }
-  list(A = A, xi = xi_tru, z = z_tru)
-}
-
-generate_nathans_data = function(n = 40,J = 60) {
-require(igraph)
-    pm1 <- cbind(c(.9, .75, .5)
-               , c(.75, .6, .25)
-               , c(.5, .25, .1))
-  pm2 <- cbind(c(.8, .1, .3)
-               , c(.1, .9, .2)
-               , c(.3, .2, .7))
-  pm3 <- cbind(c(.1, .4, .6)
-               , c(.4, .3, .1)
-               , c(.6, .1, .5))
-  
-  z_tru <- c(rep(1,J/3), rep(2,J/3), rep(3,J/3))
-  A <- xi_tru <- vector("list", length =J)
-  for (j in seq_len(J)) {
-    
-    # vary network order
-    # n <- sample(seq(60, 80, 20), 1)
-    n <- 40
-    
-    if (j <=J/3) {
-      G <- as_adj(sample_sbm(n, pm1, c(.4*n, .35*n, .25*n)))
-     xi_tru[[j]] <- c(rep(1, .4*n), rep(2, .35*n), rep(3, .25*n))
-      
-    } else if (j <= 2*J/3) {
-      G <- as_adj(sample_sbm(n, pm2, c(.7*n, .15*n, .15*n)))
-     xi_tru[[j]] <- c(rep(1, .7*n), rep(2, .15*n), rep(3, .15*n))
-      
-    } else {
-      G <- as_adj(sample_sbm(n, pm3, c(.2*n, .4*n, .4*n)))
-     xi_tru[[j]] <- c(rep(1, .2*n), rep(2, .4*n), rep(3, .4*n))
-    }
-    # unlabeled networks
-    pi <- sample(n)
-    A[[j]] <- G[pi, pi]
-   xi_tru[[j]] <-xi_tru[[j]][pi]
-  }
-  
-  list(A = A, xi = xi_tru, z = z_tru)
 }
